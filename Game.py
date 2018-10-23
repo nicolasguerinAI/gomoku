316cc211f041996700b3e13259e0a79bc8d7eb1f
Unknown <jeremiah.decombe@epitech.eu>
2018-10-23 16:24:38 +0200

    About Command

--- Game.py
+++ Game.py
@@ -1,8 +1,15 @@
 import AI
 import Board
 
+name = "pbrain"
+version = "1.0"
+author = "Nicolas GUERIN & Jeremiah DECOMBE"
+country = "France"
 
 class Game:
     def __init__(self):
         self.board = self.board.Board()
         self.ai = self.ai.AI()
+
+    def about(self):
+        print(f'name=\"{name}\", version=\"{version}\", author=\"{author}\", country=\"{country}\"')
