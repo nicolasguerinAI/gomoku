c2ee7834d762c68b222b49a01e2349f1ce4f13e3
Unknown <jeremiah.decombe@epitech.eu>
2018-10-23 16:27:52 +0200

    Global for Game Constants

--- Game.py
+++ Game.py
@@ -3,15 +3,20 @@ import Board
 from Board import Board
 from AI import AI
 
-name = "pbrain"
-version = "1.0"
-author = "Nicolas GUERIN & Jeremiah DECOMBE"
-country = "France"
+# About Constants
+NAME = "pbrain"
+VERSION = "1.0"
+AUTHOR = "Nicolas GUERIN & Jeremiah DECOMBE"
+COUNTRY = "France"
+
+# IDs Constant
+AI_ID = 1
+PLAYER_ID = 2
 
 class Game:
     def __init__(self):
         self.board = Board()
-        self.ai = AI(1, self.board)
+        self.ai = AI(AI_ID, self.board)
 
     def about(self):
-        print(f'name=\"{name}\", version=\"{version}\", author=\"{author}\", country=\"{country}\"')
+        print(f'name=\"{NAME}\", version=\"{VERSION}\", author=\"{AUTHOR}\", country=\"{COUNTRY}\"')
