57e148b0ed66d226ffc1fafcc0f14c1fba0ada55
Jeremiah DECOMBE <jeremiah.decombe@epitech.eu>
2018-10-23 11:23:31 +0200

    JDE: compile.py updated

--- compile.py
+++ compile.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 
 import subprocess
 import os
 
-subprocess.Popen("pyinstaller --onefile --windowed main.py AI.py Board.py Game.py")
-os.rename("./dist/main.exe", "./gomoku.exe")
-
+if __name__ == "__main__":
+    subprocess.call(["pyinstaller", "--onefile", "main.py", "AI.py", "Board.py", "Game.py"])
+    try:
+        os.rename("./dist/main.exe", "./gomoku.exe")
+    except:
+        os.rename("./dist/main", "./gomoku")
