26af6bcd4b153134b72d9a68e9651d597146d61f
Jeremiah DECOMBE <jeremiah.decombe@epitech.eu>
2018-10-28 17:46:57 +0100

    AI v1

--- AI.py
+++ AI.py
@@ -1,27 +1,53 @@
 import random
 import Game
-class AI:
+
+class AI(object):
+
     def __init__(self, id, board):
         self.id = id
         self.board = board
 
+    def __get_max(self, bound_min, bound_max, id):
+        max_x = 0
+        max_y = 0
+        max_score = 0
+        tmp = 0
+
+        for x in range(bound_min[0], bound_max[0]):
+            for y in range(bound_min[1], bound_max[1]):
+                if self.board.check_token(x, y) == False:
+                    continue
+                tmp = self.board.count_token(x, y, id)
+                if tmp > max_score:
+                    max_score = tmp
+                    max_x = x
+                    max_y = y
+
+        return max_x, max_y, max_score
+
+    def __play(self):
+        bound_min = [0, 0]
+        bound_max = [self.board.size, self.board.size]
+        x_ai, y_ai, score_ai = self.__get_max(bound_min, bound_max, Game.AI_ID)
+        x_player, y_player, score_player = self.__get_max(bound_min, bound_max, Game.PLAYER_ID)
+
+        if score_ai == 0 and score_player == 0:
+            x, y = random.randint(0, 18), random.randint(0, 18)
+            print(f'{x},{y}')
+        elif score_player > score_ai:
+            self.board.add_token(Game.AI_ID, x_player, y_player)
+            print(f'{x_player},{y_player}')
+        else:
+            self.board.add_token(Game.AI_ID, x_ai, y_ai)
+            print(f'{x_ai},{y_ai}')
 
     def cmd_TURN(self, parameters):
         param_list = parameters[0].split(",")
         self.board.add_token(Game.PLAYER_ID, int(param_list[0]), int(param_list[1]))
-        x, y = random.randint(0, 18), random.randint(0, 18)
-        while self.board.check_token(x, y) is False:
-            x,y = random.randint(0, 18), random.randint(0, 18)
-        self.board.add_token(Game.AI_ID, x, y)
-        print(f'{x},{y}')
+        self.__play()
 
     def cmd_BEGIN(self, parameters):
-        x, y = random.randint(0, 18), random.randint(0, 18)
-        if self.board.check_token(x, y) is True:
-            self.board.add_token(Game.AI_ID, x, y)
-            print(f'{x},{y}')
-        else:
-            self.cmd_TURN(parameters)
+        self.__play()
 
     def do_command(self, command, parameters):
         attr_name = "cmd_" + command.value
